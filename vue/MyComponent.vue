0bed77a2e0c89611bfcfdfa403e5e02a0e641948
thunthuni <codingjoha@gmail.com>
2024-05-02 17:50:19 +0900

    vue_single

---
+++ vue/MyComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <p class="greeting"> {{ msg }} </p>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue'
+  
+  const msg = ref('Hello')
+  
+  </script>
+  
+  <!-- scoped는 현재 이 컴포넌트에서만 적용하겠다는 뜻 -->
+  <style scoped>
+    .greeting {
+        color: crimson;
+    }
+  </style>
